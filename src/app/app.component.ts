59debe7ad47493d91eec3700a67e12683b5c48ae
Alexandra <nimrod13@users.noreply.github.com>
2020-03-21 16:47:07 +0100

    Partial fix for first click on heroes view

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Marvel Heroes';
+
+  onButtonGroupClick($event) {
+    const clickedElement = $event.target || $event.srcElement;
+
+    if (clickedElement.nodeName === 'BUTTON') {
+
+      const isActive = clickedElement.parentElement.querySelector('.active');
+      // if a Button already has Class: .active
+      if (isActive) {
+        isActive.classList.remove('active');
+      }
+
+      clickedElement.className += ' active';
+    }
+
+  }
 }
